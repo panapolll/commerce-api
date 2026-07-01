c3bc104d06869dfeac6e969a334f0ba1d2aa4bcb
Giss <panaponsukcharoen@email.com>
2026-07-01 19:25:31 +0700

    sync latest code

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,11 @@ dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
